4d03972a7df19c02a552a25a96573881cd59ca48
Nicolas hc <nicolashidcas@gmail.com>
2022-07-01 22:36:12 -0400

    :test_tube: Added example service to unit test in example controller

--- src/modules/example/example.controller.spec.ts
+++ src/modules/example/example.controller.spec.ts
@@ -1,5 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ExampleController } from './example.controller';
+import { ExampleService } from './example.service';
 
 describe('ExampleController', () => {
   let controller: ExampleController;
@@ -7,6 +8,7 @@ describe('ExampleController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [ExampleController],
+      providers: [ExampleService]
     }).compile();
 
     controller = module.get<ExampleController>(ExampleController);
